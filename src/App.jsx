ef60fdc33a3a9fa49b3028d4063916d6646e0f0a
CodeBustler <codebustler360online@gmail.com>
2023-11-28 20:32:38 +0530

    DEPLOY

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import SearchBar from "./components/SearchBar";
+import MovieCard from "./components/MovieCard";
+import Navbar from "./components/Navbar";
+
+function App() {
+  const [allMovieData, setAllMovieData] = useState([]);
+  const [searchMovie, setSearchMovie] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // Function to fetch data with debouncing
+  const fetchMovieData = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(
+        `https://omdbapi.com/?s=${searchMovie}&apikey=112b26a6`,
+      );
+      const data = await res.json();
+      setAllMovieData(data.Search);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  console.log(allMovieData);
+  return (
+    <div className="bg">
+      <Navbar />
+      <SearchBar
+        searchMovie={searchMovie}
+        setSearchMovie={setSearchMovie}
+        fetchMovieData={fetchMovieData}
+      />
+      <MovieCard allMovieData={allMovieData} loading={loading} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function SearchBar({ searchMovie, setSearchMovie, fetchMovieData }) {
+	return (
+		<div className="searchBox">
+			<input
+				type="text"
+				placeholder="Search here"
+				value={searchMovie}
+				onChange={(e) => {
+					setSearchMovie(e.target.value);
+				}}
+				autoFocus
+			/>
+			<button onClick={fetchMovieData}>
+				<i className="fa-solid fa-magnifying-glass"></i>
+			</button>
+		</div>
+	);
+}
+
+export default SearchBar;
